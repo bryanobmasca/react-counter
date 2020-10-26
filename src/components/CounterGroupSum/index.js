99ea20d11b5dbfb4d100d2e7eb18b12529789bff
bryanobmasca <bryanobmasca@gmail.com>
2020-10-26 21:31:15 +0800

    [React][Bryan] feature: Add CounterGroupSum Class

---
+++ src/components/CounterGroupSum/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CounterGroupSum extends Component {
+
+    render() {
+        return (
+            <div>
+                <label htmlFor="">Sum: {this.props.sum}</label>
+            </div>
+        );
+    }
+}
+
+export default CounterGroupSum;
