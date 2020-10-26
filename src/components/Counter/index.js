15f6c05e0479d53c6dace0c5d3644652d5a971fc
bryanobmasca <bryanobmasca@gmail.com>
2020-10-26 18:20:23 +0800

    [React][Bryan] feature: Add increaseValue method

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.increaseValue = this.increaseValue.bind(this);
 
+        this.state = {
+            number: 0
+        };
+    }
+    
+    increaseValue(){
+        this.setState((prevState) => ({
+            number: prevState.number + 1
+        }));
+    }
     render() {
         return (
             <section>
-                <input type="button" value="+"/>
-                <input type="number" id="number" value="0"/>
+                <input type="button" value="+" onClick={this.increaseValue}/>
+                <input type="number" id="number" value={this.state.number}/>
                 <input type="button" value="-"/>
             </section>
         );
